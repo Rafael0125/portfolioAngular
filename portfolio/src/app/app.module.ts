b8aef2885f94a5143c3c05eac2832b19b4b74d3b
Rafael0125 <rafanavarro0125@gmail.com>
2023-04-12 20:47:38 -0300

    Criação dos componentes, e do template do menu

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApresentacaoComponent } from './apresentacao/apresentacao.component';
+import { MenuComponent } from './menu/menu.component';
+import { IntroducaoComponent } from './apresentacao/introducao/introducao.component';
+import { SobreMimComponent } from './apresentacao/sobre-mim/sobre-mim.component';
+import { ProjetosComponent } from './projetos/projetos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApresentacaoComponent,
+    MenuComponent,
+    IntroducaoComponent,
+    SobreMimComponent,
+    ProjetosComponent,
   ],
   imports: [
     BrowserModule,

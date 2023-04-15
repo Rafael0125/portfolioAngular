ab12f33a1ee8552b49e668e91ca860d0f5807cbb
Rafael0125 <rafanavarro0125@gmail.com>
2023-04-15 12:32:08 -0300

    configurado o favicon do site

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,11 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ApresentacaoComponent } from './apresentacao/apresentacao.component';
-import { SobreMimComponent } from './apresentacao/sobre-mim/sobre-mim.component';
-
-import { Projeto } from './projetos/projeto.model';
-
 const routes: Routes = [];
 
 @NgModule({

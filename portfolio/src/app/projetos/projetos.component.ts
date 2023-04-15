1e9538f857c843bc9cb5e27eb9d3adc7515d2aa8
Rafael0125 <rafanavarro0125@gmail.com>
2023-04-15 13:44:26 -0300

    V3 criada

--- portfolio/src/app/projetos/projetos.component.ts
+++ portfolio/src/app/projetos/projetos.component.ts
@@ -1,5 +1,6 @@
-import { Component,OnInit, ViewChildren,QueryList, ElementRef,AfterViewInit } from '@angular/core';
+import { Component,OnInit, ViewChild, ViewChildren,QueryList, ElementRef, AfterViewInit } from '@angular/core';
 import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 import { Projeto } from './projeto.model';
 import { PROJETOS } from './projeto.mock';
@@ -14,19 +15,16 @@ export class ProjetosComponent implements OnInit{
   @ViewChildren('carouselIcones')
   public carouselIcones!:QueryList<ElementRef>
 
-
-
   public projetos:Projeto[] = PROJETOS
 
   constructor(){ }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
   ngAfterViewInit(): void {
 
     this.carouselIcones.forEach((carouselIconesDiv:ElementRef<HTMLDivElement>) => {
-      gsap.from(carouselIconesDiv.nativeElement,{rotation:0,x:-300,duration:10,delay:0}) // icones Linguagens
+      gsap.from(carouselIconesDiv.nativeElement,{rotation:0,x:-200,duration:10,delay:0}) // icones Linguagens
       gsap.to(carouselIconesDiv.nativeElement,{rotation:0,x:750,duration:10,delay: 0,repeat:-1, ease:'in-out'})
     })
 
